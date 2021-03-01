1f86c7121320e2efee19f4df74123e80a2df0e6b
dm67x <dm67x@outlook.fr>
2021-03-01 19:47:44 +0100

    Update database implementation

--- src/database.rs
+++ src/database.rs
@@ -1,39 +1,49 @@
+use crate::error::FailureKind;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 lazy_static! {
-    pub static ref SQLITE: Pool<SqliteConnectionManager> = {
+    static ref SQLITE: Pool<SqliteConnectionManager> = {
         let sqlite = SqliteConnectionManager::file("passguard.db");
-        let pool = Pool::new(sqlite).unwrap();
-        pool.get()
-            .unwrap()
-            .execute(
-                r#"
-                create table if not exists users (
-                    id integer primary key,
-                    name text not null,
-                    password text not null
-                )
-                "#,
-                params![],
-            )
-            .unwrap();
-        pool.get()
-            .unwrap()
-            .execute(
-                r#"
-                create table if not exists passwords (
-                    id integer primary key,
-                    website text not null,
-                    password text not null,
-                    user_id id integer not null,
-                    foreign key(user_id) references users(id)
-                )
-                "#,
-                params![],
-            )
-            .unwrap();
-        pool
+        Pool::new(sqlite).unwrap()
     };
+    static ref INITIALIZED: AtomicBool = AtomicBool::new(false);
+}
+
+fn init(pool: &Pool<SqliteConnectionManager>) -> Result<(), FailureKind> {
+    log::info!("Called");
+    pool.get()?.execute(
+        r#"
+        create table if not exists users (
+            id integer primary key,
+            name text not null,
+            password text not null
+        )
+        "#,
+        params![],
+    )?;
+    pool.get()?.execute(
+        r#"
+        create table if not exists passwords (
+            id integer primary key,
+            website text not null,
+            password text not null,
+            user_id id integer not null,
+            foreign key(user_id) references users(id)
+        )
+        "#,
+        params![],
+    )?;
+    Ok(())
+}
+
+pub fn get() -> Result<&'static Pool<SqliteConnectionManager>, FailureKind> {
+    let pool = &*SQLITE;
+    let initialized = INITIALIZED.swap(true, Ordering::SeqCst);
+    if !initialized {
+        init(pool)?;
+    }
+    Ok(pool)
 }
