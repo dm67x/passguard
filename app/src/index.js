7958ca7baa82a2cb066a2b23b08061af50bac2f6
dm67x <dm67x@outlook.fr>
2021-03-21 14:53:40 +0100

    remove webpack

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -18,7 +19,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
